e0ee5583c1f880ef0e4e2d63dbafdf1b767977ef
dthach41 <thachderek1128@gmail.com>
2024-10-23 13:05:58 -0400

    First Commit

---
+++ Sports Meetup App/First Screen/FirstScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  FirstScreenView.swift
+//  Sports Meetup App
+//
+//  Created by Derek Thach on 10/23/24.
+//
+
+import UIKit
+
+class FirstScreenView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
